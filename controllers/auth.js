0e42cb15e900ec246e04e2fa781a9b61b13e849e
vivek-0921 <vivekpansheriya4@gmail.com>
2025-03-17 17:12:51 +0530

    first register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,29 +1,30 @@
 
 
 const userModel = require('../models/user');
+const bcrypt = require('bcrypt');
 
 async function register(req, res) {
     const { email, password } = req.body;
-    const user = await userModel.create({ email, password });
-    res.json({
-        status: 200,
-        message: 'user created',
-        data: user
-    });
-
-    // const payload = req.body
-
-    // const hashpassword = await bcrypt.hash(req.body.password, 10);
-    // const hashpassword = bcrypt.hashSync(payload.password, 10);
-    // const user = await usermodel.create({
-    //     ...payload, password: hashpassword
-    // })
-    // // res.redirect('/')
+    // const user = await userModel.create({ email, password });
     // res.json({
     //     status: 200,
     //     message: 'user created',
     //     data: user
-    // })
+    // });
+
+    const payload = req.body
+
+    const hashpassword = await bcrypt.hash(req.body.password, 10);
+    // const hashpassword = bcrypt.hashSync(payload.password, 10);
+    const user = await userModel.create({
+        ...payload, password: hashpassword
+    })
+    // res.redirect('/')
+    res.json({
+        status: 200,
+        message: 'user created',
+        data: user
+    })
 }
 
 async function login(req, res) {
@@ -32,7 +33,7 @@ async function login(req, res) {
     if (!user) return res.json({ status: 404, message: 'user not found' });
     const isvalid = await bcrypt.compare(password, user.password);
     if (!isvalid) return res.json({ status: 400, message: 'invalid password' });
-    
+
 
 }
 
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 
 const dbconnections = require('./configs/db');
 const userRouter = require('./routes/user');
@@ -9,8 +10,8 @@ const authRouter = require('./routes/auth');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 // app.use(express.urlencoded({ exauthtended: false }));
-
 
 dbconnections();
 
